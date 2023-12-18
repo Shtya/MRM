3c6c2b4b5981c0becfa28a921dbbf18b3c611e08
Shtya <Shtya54@gmail.com>
2023-12-18 13:47:41 +0200

    6

--- src/API/API.js
+++ src/API/API.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const Api = 'http://127.0.0.1:5000/'
+const Api = 'https://mrm-server.onrender.com/'
 
 const baseURL = axios.create({baseURL : `${Api}api/posts/`})
 export const baseImg = `${Api}`
